376dd7871698b8276a5c9eca0d65f53f2517c4d0
Philip Glebow <philip_glebow@gap.com>
2016-01-24 19:33:27 +0530

    Enhancements

---
+++ src/main/java/com/glebow/proto/OptimizationProcessor.java
@@ -0,0 +1,135 @@
+/**
+ * 
+ */
+package com.glebow.proto;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Iterators;
+import com.google.common.collect.SetMultimap;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.sun.istack.internal.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author pglebow
+ *
+ */
+@Slf4j
+public class OptimizationProcessor<H extends Comparable<H>> {
+
+    @NotNull
+    protected Collection<H> hosts;
+
+    @NotNull
+    protected Collection<Task> tasks;
+
+    @NotNull
+    protected int workersPerHost;
+
+    /** Executor for the overall process */
+    protected ListeningExecutorService pool = MoreExecutors.listeningDecorator(Executors.newSingleThreadExecutor());
+
+    /**
+     * Constructor
+     * 
+     * @param hosts
+     * @param workersPerHost
+     * @param numberOfTasks
+     */
+    public OptimizationProcessor(Collection<H> hosts, int workersPerHost, Collection<Task> tasks) {
+        this.hosts = hosts;
+        this.workersPerHost = workersPerHost;
+        this.tasks = tasks;
+    }
+
+    /**
+     * Processes the tasks on each of the hosts and returns once the process is
+     * complete
+     * 
+     * @throws InterruptedException
+     */
+    public ListenableFuture<Duration> process() throws InterruptedException {
+        return pool.submit(new ProcessingTask(LocalDateTime.now()));
+    }
+
+    @AllArgsConstructor
+    protected class ProcessingTask implements Callable<Duration> {
+        private LocalDateTime start;
+
+        @Override
+        public Duration call() throws Exception {
+            SetMultimap<H, Task> tasksForHost = buildSetMultimap();
+
+            CountDownLatch latch = new CountDownLatch(tasksForHost.keySet().size());
+
+            for (Map.Entry<H, Collection<Task>> e : tasksForHost.asMap().entrySet()) {
+                H host = e.getKey();
+
+                ThreadFactoryBuilder b = new ThreadFactoryBuilder();
+                b.setNameFormat(host + "-worker-%d");
+                NamedThreadPoolExecutor executor = new NamedThreadPoolExecutor(workersPerHost, workersPerHost, 0L,
+                        TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), b.build());
+                executor.setName(host.toString());
+                executor.setLatch(latch);
+
+                ListeningExecutorService les = MoreExecutors.listeningDecorator(executor);
+
+                for (Task task : e.getValue()) {
+                    Futures.addCallback(les.submit(task), new FutureCallback<Result>() {
+
+                        @Override
+                        public void onSuccess(Result result) {
+                            log.info("Success: " + result.toString());
+                        }
+
+                        @Override
+                        public void onFailure(Throwable t) {
+                            log.info("Failure: " + t.getMessage());
+                        }
+                    });
+                }
+                les.shutdown();
+            }
+            latch.await();
+            return Duration.between(start, LocalDateTime.now());
+        }
+
+    }
+
+    /**
+     * Constructs a Multimap where the key is the host and the value is a
+     * collection of tasks
+     * 
+     * @return SetMultimap<H, T>
+     */
+    private SetMultimap<H, Task> buildSetMultimap() {
+        SetMultimap<H, Task> tasksForHost;
+
+        tasksForHost = HashMultimap.create(hosts.size(),
+                (tasks.size() / workersPerHost) + (tasks.size() % workersPerHost));
+
+        Iterator<H> hostIterator = Iterators.cycle(hosts);
+        tasks.forEach(t -> tasksForHost.put(hostIterator.next(), t));
+
+        return tasksForHost;
+    }
+
+}
--- src/test/java/com/glebow/proto/ConcurrencyTest.java
+++ src/test/java/com/glebow/proto/ConcurrencyTest.java
@@ -3,88 +3,43 @@
  */
 package com.glebow.proto;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Test;
 
-import com.google.common.collect.Lists;
-import com.google.common.util.concurrent.FutureCallback;
-import com.google.common.util.concurrent.Futures;
-import com.google.common.util.concurrent.ListeningExecutorService;
-import com.google.common.util.concurrent.MoreExecutors;
-import com.google.common.util.concurrent.ThreadFactoryBuilder;
+import com.google.common.collect.Sets;
+
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author pglebow
  *
  */
+@Slf4j
 public class ConcurrencyTest {
 
-    private CountDownLatch latch;
-    private LinkedList<String> hosts = Lists.newLinkedList();
     private int workersPerHost = 4;
-    private int numberOfTasks = 2001;
 
-    private List<String> toProcess;
+    private int numTasks = 17;
+
+    private Set<String> hosts = Sets.newHashSet("One", "Two", "Three", "Four");
 
     @Test
-    public void test() throws InterruptedException {
-
-        toProcess = new ArrayList<>(numberOfTasks);
-        for (int i = 0; i < numberOfTasks; i++) {
-            toProcess.add(String.valueOf(i));
-        }
-
-        hosts.add("One");
-        hosts.add("Two");
-        hosts.add("Three");
-        hosts.add("Four");
-
-        latch = new CountDownLatch(hosts.size());
-
-        LinkedList<ListeningExecutorService> executors = new LinkedList<>();
-        hosts.forEach(h -> {
-            ThreadFactoryBuilder b = new ThreadFactoryBuilder();
-            b.setNameFormat(h + "-worker-%d");
-            NamedThreadPoolExecutor e = new NamedThreadPoolExecutor(workersPerHost, workersPerHost, 0L,
-                    TimeUnit.MILLISECONDS, new LinkedBlockingQueue<Runnable>(), b.build());
-            e.setName(h);
-            e.setLatch(latch);
-            executors.add(MoreExecutors.listeningDecorator(e));
-        });
-
-        List<List<String>> partitionedTasks = Lists.partition(toProcess, (toProcess.size() / workersPerHost));
-        for (int i = 0; i < hosts.size(); i++) {
-            ListeningExecutorService e = executors.get(i);
-            for (int j = 0; j < workersPerHost; j++) {
-                List<String> tasks = partitionedTasks.get(j);
-
-                tasks.forEach(t -> {
-                    System.out.println("Creating task " + t);
-                    Futures.addCallback(e.submit(new Task(t)), new FutureCallback<Result>() {
-
-                        @Override
-                        public void onSuccess(Result result) {
-                            System.out.println("Success: " + result.toString());
-                        }
-
-                        @Override
-                        public void onFailure(Throwable t) {
-                            System.out.println("Failure: " + t.getMessage());
-                        }
-                    });
-                });
+    public void test() {
+        try {
+            Set<Task> tasks = new HashSet<Task>();
+            for (int i = 0; i < numTasks; i++) {
+                tasks.add(new Task(String.valueOf(i)));
             }
-            e.shutdown();
-        }
 
-        latch.await();
-        System.out.println("Process complete!");
+            OptimizationProcessor<String> p = new OptimizationProcessor<String>(hosts, workersPerHost, tasks);
+            Duration d = p.process().get();
+            log.info("Duration: " + d);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
     }
 
 }
